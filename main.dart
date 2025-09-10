4fd710606d3696da5a754d478c8fb894e945dc87
GTCSanmar <136872259+GTCSanmar@users.noreply.github.com>
2025-09-10 13:05:08 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,13 +1,5 @@
-void main() {
-  // Declaração das variáveis
-  String primeiroNome = "Ana";
-  String sobrenome = "Silva";
-
-  // Usando concatenação
-  String nomeCompletoConcatenado = primeiroNome + " " + sobrenome;
-  print("Usando concatenação: $nomeCompletoConcatenado");
-
-  // Usando interpolação de string
-  String nomeCompletoInterpolado = "$primeiroNome $sobrenome";
-  print("Usando interpolação: $nomeCompletoInterpolado");
-}
+SELECT 
+    email,
+    SUBSTR(email, 1, INSTR(email, '@') - 1) AS login
+FROM 
+    tabela_emails;
